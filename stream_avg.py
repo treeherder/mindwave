a83af39a6434c85741e190a23d446aa6bb62a25a
Brendan Reddy-Best <b.d.reddybest@gmail.com>
2013-10-09 01:02:44 -0700

    Create stream_avg.py

---
+++ stream_avg.py
@@ -0,0 +1,26 @@
+import mindwave, time
+
+P = 100
+
+def s_avg(N):
+  stream = []
+  stream.append(N)
+  if len(stream) > P:
+    # Only average the last P elements of the stream
+    del(stream[:1])
+  elif len(stream) == 0:
+    average = 0
+  else:
+    sum = 0
+    for n in stream:
+      sum = sum + n
+      average = sum / len(stream)
+  return (average)
+
+headset = mindwave.Headset('/dev/tty.MindWaveMobile-DevA', '9402')
+headset.disconnect()  #clear any previous activity
+headset.connect()
+
+while True:
+ print( "attention average :   {0}\r\n".format(s_avg(headset.attention)))
+ print( "meditation average:   {0}\r\n".format(s_avg(headset.meditation)))
